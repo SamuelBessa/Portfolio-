beb1cf467a8baa3f0397a3cd95a53898525f8d17
Samuel Bessa <Dev.samuelbessa@gmail.com>
2024-11-09 23:39:49 -0300

    add os icones das tecnologias

--- src/componentes/tecnologias/tecnologias.jsx
+++ src/componentes/tecnologias/tecnologias.jsx
@@ -1,50 +1,73 @@
-import Outono from "../projetos/img/Suíça.jpg"
 import "./tecnologias.css"
 
 export function Tecnologias() {
 
     const Tecnologias = {
         html: {
-            nome: "HTML5",
-            img: Outono,
+            nome: "HTML",
+            img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
             sobre: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat aperiam perspiciatis neque iusto id porro dicta reprehenderit, eos laudantium molestiae est, saepe animi cupiditate accusamus deleniti, expedita vel in quidem.",
         },
 
         css: {
             nome: "CSS",
-            img: Outono,
+            img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
             sobre: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat aperiam perspiciatis neque iusto id porro dicta reprehenderit, eos laudantium molestiae est, saepe animi cupiditate accusamus deleniti, expedita vel in quidem.",
         },
 
         javascript: {
             nome: "JavaScript",
-            img: Outono,
+            img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
             sobre: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat aperiam perspiciatis neque iusto id porro dicta reprehenderit, eos laudantium molestiae est, saepe animi cupiditate accusamus deleniti, expedita vel in quidem.",
         },
 
         git: {
             nome: "Git",
-            img: Outono,
+            img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg",
             sobre: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat aperiam perspiciatis neque iusto id porro dicta reprehenderit, eos laudantium molestiae est, saepe animi cupiditate accusamus deleniti, expedita vel in quidem.",
         },
 
         react: {
             nome: "React",
-            img: Outono,
+            img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
             sobre: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat aperiam perspiciatis neque iusto id porro dicta reprehenderit, eos laudantium molestiae est, saepe animi cupiditate accusamus deleniti, expedita vel in quidem.",
         },
 
         typescript: {
             nome: "TypeScript",
-            img: Outono,
+            img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg",
             sobre: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat aperiam perspiciatis neque iusto id porro dicta reprehenderit, eos laudantium molestiae est, saepe animi cupiditate accusamus deleniti, expedita vel in quidem.",
         },
 
         jquery: {
             nome: "JQuery",
-            img: Outono,
+            img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jquery/jquery-original.svg",
             sobre: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat aperiam perspiciatis neque iusto id porro dicta reprehenderit, eos laudantium molestiae est, saepe animi cupiditate accusamus deleniti, expedita vel in quidem.",
-        }
+        },
+
+        redux: {
+            nome: "Redux",
+            img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/redux/redux-original.svg",
+            sobre: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat aperiam perspiciatis neque iusto id porro dicta reprehenderit, eos laudantium molestiae est, saepe animi cupiditate accusamus deleniti, expedita vel in quidem.",
+        },
+
+        jest: {
+            nome: "Jest",
+            img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jest/jest-plain.svg",
+            sobre: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat aperiam perspiciatis neque iusto id porro dicta reprehenderit, eos laudantium molestiae est, saepe animi cupiditate accusamus deleniti, expedita vel in quidem.",
+        },
+
+        github: {
+            nome: "GitHub",
+            img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg",
+            sobre: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat aperiam perspiciatis neque iusto id porro dicta reprehenderit, eos laudantium molestiae est, saepe animi cupiditate accusamus deleniti, expedita vel in quidem.",
+        },
+
+        figma: {
+            nome: "Figma",
+            img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg",
+            sobre: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat aperiam perspiciatis neque iusto id porro dicta reprehenderit, eos laudantium molestiae est, saepe animi cupiditate accusamus deleniti, expedita vel in quidem.",
+        },
     }
 
     return (
