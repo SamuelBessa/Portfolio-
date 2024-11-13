1b7ec1425f512676b824a06ad3c89c464a77ea15
Samuel Bessa <Dev.samuelbessa@gmail.com>
2024-11-13 18:03:18 -0300

    add os texto na section sobre mim

--- src/componentes/section-sobre-mim/sobre-mim.jsx
+++ src/componentes/section-sobre-mim/sobre-mim.jsx
@@ -6,12 +6,13 @@ export function SobreMin() {
             <div className="conteiner">
                 <div className="sobre-mim">
                     <h2>Sobre Mim</h2>
-                    <p>Olá! Eu sou Samuel Bessa, um entusiasta do desenvolvimento frontend e alguém que sempre busca transformar ideias em experiências digitais inovadoras. Nos meus momentos livres, gosto de aprender sobre segurança digital e explorar novas tecnologias, pois acredito que isso me ajuda a manter uma visão atualizada e completa sobre o que posso oferecer como profissional. Além disso, tenho o sonho de morar na Europa, onde espero mergulhar em novas culturas e ampliar minha visão sobre o mundo e a tecnologia.</p>
+                    <p>Olá! Eu sou Samuel Bessa, um desenvolvedor frontend apaixonado por transformar ideias em experiências digitais inovadoras. Nos meus momentos livres, gosto de explorar novas tecnologias e aprender sobre segurança digital, pois acredito que isso amplia minha visão e me ajuda a oferecer soluções mais completas e seguras. Além disso, tenho o sonho de conhecer a Europa, onde espero mergulhar em novas culturas e expandir minha perspectiva sobre o mundo e a tecnologia.
+                    </p>
                 </div>
 
                 <div className="experiencia-tecnica">
                     <h2>Minhas Experiência Técnica</h2>
-                    <p>Minha principal especialidade é o desenvolvimento frontend usando React, além de sólidos conhecimentos em JavaScript, HTML e CSS. Tenho experiência com projetos diversos, desde sites institucionais até aplicações interativas, focando sempre em usabilidade e performance. Estou constantemente aprimorando meu trabalho, e atualmente, estou estudando mais sobre o Vite, buscando otimizar a performance das minhas aplicações em React. Meu objetivo é continuar evoluindo como desenvolvedor e contribuir para projetos que façam diferença no dia a dia das pessoas.                           </p>
+                    <p>Minha principal especialidade é o desenvolvimento frontend com React, além de sólidos conhecimentos em HTML, CSS e JavaScript. Tenho experiência em projetos variados, desde sites institucionais até aplicações interativas, com foco em usabilidade, performance e design responsivo. Recentemente, estou aprofundando meus estudos em Vite para otimizar a performance das minhas aplicações React, visando sempre a entrega de soluções eficientes. Meu objetivo é continuar evoluindo como desenvolvedor e colaborar em projetos que tenham impacto positivo e relevância.</p>
                 </div>
             </div>
         </section>
