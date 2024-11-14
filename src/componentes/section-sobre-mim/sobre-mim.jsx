bcad98a1168f19f3158fc20a224a3a3934c79bc2
Samuel Bessa <Dev.samuelbessa@gmail.com>
2024-11-13 22:06:05 -0300

    trocando o nomes das seções

--- src/componentes/section-sobre-mim/sobre-mim.jsx
+++ src/componentes/section-sobre-mim/sobre-mim.jsx
@@ -5,13 +5,13 @@ export function SobreMin() {
         <section id="sobre">
             <div className="conteiner">
                 <div className="sobre-mim">
-                    <h2>Sobre Mim</h2>
+                    <h2>Sobre mim</h2>
                     <p>Olá! Eu sou Samuel Bessa, um desenvolvedor frontend apaixonado por transformar ideias em experiências digitais inovadoras. Nos meus momentos livres, gosto de explorar novas tecnologias e aprender sobre segurança digital, pois acredito que isso amplia minha visão e me ajuda a oferecer soluções mais completas e seguras. Além disso, tenho o sonho de conhecer a Europa, onde espero mergulhar em novas culturas e expandir minha perspectiva sobre o mundo e a tecnologia.
                     </p>
                 </div>
 
                 <div className="experiencia-tecnica">
-                    <h2>Minhas Experiência Técnica</h2>
+                    <h2>Minhas experiência técnica</h2>
                     <p>Minha principal especialidade é o desenvolvimento frontend com React, além de sólidos conhecimentos em HTML, CSS e JavaScript. Tenho experiência em projetos variados, desde sites institucionais até aplicações interativas, com foco em usabilidade, performance e design responsivo. Recentemente, estou aprofundando meus estudos em Vite para otimizar a performance das minhas aplicações React, visando sempre a entrega de soluções eficientes. Meu objetivo é continuar evoluindo como desenvolvedor e colaborar em projetos que tenham impacto positivo e relevância.</p>
                 </div>
             </div>
